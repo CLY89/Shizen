f22200cf45c71483733a20438701f091a71f2420
CLY89 <yatsuku200x@gmail.com>
2020-11-04 22:46:40 +0900

    Add tree items

--- src/main/java/net/docv/shizen/item/Items.java
+++ src/main/java/net/docv/shizen/item/Items.java
@@ -24,23 +24,46 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = Shizen.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Items {
 	public static List<Item> itemList = new ArrayList<Item>();
-	public static final Item BANANA = register("banana", new Banana());
-	public static final Item APRICOT = register("apricot", new Apricot());
-	public static final Item PEACH = register("peach", new Peach());
-	public static final Item PLUM = register("plum", new Plum());
-	public static final Item PEAR = register("pear", new Pear());
-	public static final Item ORANGE = register("orange", new Orange());
-	public static final Item GRAPE = register("grape", new Grape());
-	public static final Item LEMON = register("lemon", new Lemon());
 
-	public static final Item BANANA_TREE_BLOCK = register("banana_tree_block", Blocks.BANANA_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item APRICOT = register("apricot", new Apricot());
 	public static final Item APRICOT_TREE_BLOCK = register("apricot_tree_block", Blocks.APRICOT_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item APRICOT_TREE_LOG_BLOCK = register("apricot_tree_log_block", Blocks.APRICOT_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item APRICOT_TREE_LEAVES_BLOCK = register("apricot_tree_leaves_block", Blocks.APRICOT_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
+
+	public static final Item BANANA = register("banana", new Banana());
+	public static final Item BANANA_TREE_BLOCK = register("banana_tree_block", Blocks.BANANA_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item BANANA_TREE_LOG_BLOCK = register("banana_tree_log_block", Blocks.BANANA_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item BANANA_TREE_LEAVES_BLOCK = register("banana_tree_leaves_block", Blocks.BANANA_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
+
+	public static final Item GRAPE = register("grape", new Grape());
 	public static final Item GRAPE_TREE_BLOCK = register("grape_tree_block", Blocks.GRAPE_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
-	public static final Item LEMON_TREE_BLOCK = register("olive_tree_block", Blocks.LEMON_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
-	public static final Item ORANGE_TREE_BLOCK = register("orange_tree_block", Blocks.ORANGE_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item GRAPE_TREE_LOG_BLOCK = register("grape_tree_log_block", Blocks.GRAPE_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item GRAPE_TREE_LEAVES_BLOCK = register("grape_tree_leaves_block", Blocks.GRAPE_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
+
+	public static final Item LEMON = register("lemon", new Lemon());
+	public static final Item LEMON_TREE_BLOCK = register("lemon_tree_block", Blocks.LEMON_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item LEMON_TREE_LOG_BLOCK = register("lemon_tree_log_block", Blocks.LEMON_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item LEMON_TREE_LEAVES_BLOCK = register("lemon_tree_leaves_block", Blocks.LEMON_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
+
+	public static final Item PEACH = register("peach", new Peach());
 	public static final Item PEACH_TREE_BLOCK = register("peach_tree_block", Blocks.PEACH_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item PEACH_TREE_LOG_BLOCK = register("peach_tree_log_block", Blocks.PEACH_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item PEACH_TREE_LEAVES_BLOCK = register("peach_tree_leaves_block", Blocks.PEACH_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
+
+	public static final Item ORANGE = register("orange", new Orange());
+	public static final Item ORANGE_TREE_BLOCK = register("orange_tree_block", Blocks.ORANGE_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item ORANGE_TREE_LOG_BLOCK = register("orange_tree_log_block", Blocks.ORANGE_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item ORANGE_TREE_LEAVES_BLOCK = register("orange_tree_leaves_block", Blocks.ORANGE_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
+
+	public static final Item PEAR = register("pear", new Pear());
 	public static final Item PEAR_TREE_BLOCK = register("pear_tree_block", Blocks.PEAR_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item PEAR_TREE_LOG_BLOCK = register("pear_tree_log_block", Blocks.PEAR_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item PEAR_TREE_LEAVES_BLOCK = register("pear_tree_leaves_block", Blocks.PEAR_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
+
+	public static final Item PLUM = register("plum", new Plum());
 	public static final Item PLUM_TREE_BLOCK = register("plum_tree_block", Blocks.PLUM_TREE_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item PLUM_TREE_LOG_BLOCK = register("plum_tree_log_block", Blocks.PLUM_TREE_LOG_BLOCK, ItemGroup.BUILDING_BLOCKS);
+	public static final Item PLUM_TREE_LEAVES_BLOCK = register("plum_tree_leaves_block", Blocks.PLUM_TREE_LEAVES_BLOCK, ItemGroup.BUILDING_BLOCKS);
 
 	private static Item register(String key, Item itemIn) {
 		itemList.add(itemIn);

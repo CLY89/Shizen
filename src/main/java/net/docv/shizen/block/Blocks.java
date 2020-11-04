e6fb137d33aed3e74c875cd336fcbcbc3170a78d
CLY89 <yatsuku200x@gmail.com>
2020-11-04 22:19:42 +0900

    Add tree log blocks

--- src/main/java/net/docv/shizen/block/Blocks.java
+++ src/main/java/net/docv/shizen/block/Blocks.java
@@ -6,28 +6,30 @@ import java.util.List;
 import net.docv.shizen.Shizen;
 import net.docv.shizen.block.trees.ApricotTreeBlock;
 import net.docv.shizen.block.trees.ApricotTreeLeavesBlock;
+import net.docv.shizen.block.trees.ApricotTreeLogBlock;
 import net.docv.shizen.block.trees.BananaTreeBlock;
 import net.docv.shizen.block.trees.BananaTreeLeavesBlock;
+import net.docv.shizen.block.trees.BananaTreeLogBlock;
 import net.docv.shizen.block.trees.GrapeTreeBlock;
 import net.docv.shizen.block.trees.GrapeTreeLeavesBlock;
+import net.docv.shizen.block.trees.GrapeTreeLogBlock;
 import net.docv.shizen.block.trees.LemonTreeBlock;
 import net.docv.shizen.block.trees.LemonTreeLeavesBlock;
+import net.docv.shizen.block.trees.LemonTreeLogBlock;
 import net.docv.shizen.block.trees.OrangeTreeBlock;
 import net.docv.shizen.block.trees.OrangeTreeLeavesBlock;
+import net.docv.shizen.block.trees.OrangeTreeLogBlock;
 import net.docv.shizen.block.trees.PeachTreeBlock;
 import net.docv.shizen.block.trees.PeachTreeLeavesBlock;
+import net.docv.shizen.block.trees.PeachTreeLogBlock;
 import net.docv.shizen.block.trees.PearTreeBlock;
 import net.docv.shizen.block.trees.PearTreeLeavesBlock;
+import net.docv.shizen.block.trees.PearTreeLogBlock;
 import net.docv.shizen.block.trees.PlumTreeBlock;
 import net.docv.shizen.block.trees.PlumTreeLeavesBlock;
-import net.minecraft.block.AbstractBlock;
+import net.docv.shizen.block.trees.PlumTreeLogBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.LeavesBlock;
-import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.block.SaplingBlock;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraftforge.event.RegistryEvent;
@@ -38,30 +40,37 @@ import net.minecraftforge.fml.common.Mod;
 public class Blocks {
 	public static List<Block> blockList = new ArrayList<Block>();
 	public static Block BANANA_TREE_BLOCK = register("banana_tree_block", new BananaTreeBlock());
+	public static Block BANANA_TREE_LOG = register("banana_tree_log", new BananaTreeLogBlock());
 	public static Block BANANA_TREE_LEAVES = register("banana_tree_leaves", new BananaTreeLeavesBlock());
 
 	public static Block APRICOT_TREE_BLOCK = register("apricot_tree_block", new ApricotTreeBlock());
-	//public static Block APRICOT_TREE_LOG = register("apricot_tree_log", new RotatedPillarBlock(AbstractBlock.Properties.create(Material.WOOD, MaterialColor.WOOD).hardnessAndResistance(2.0F).sound(SoundType.WOOD)));
+	public static Block APRICOT_TREE_LOG = register("apricot_tree_log", new ApricotTreeLogBlock());
 	public static Block APRICOT_TREE_LEAVES = register("apricot_tree_leaves", new ApricotTreeLeavesBlock());
 	//public static Block APRICOT_TREE_SAPLING = register("apricot_tree_sapling", new SaplingBlock(new ApricotTree(), AbstractBlock.Properties.create(Material.PLANTS).doesNotBlockMovement().tickRandomly().zeroHardnessAndResistance().sound(SoundType.PLANT)));
 
 	public static Block GRAPE_TREE_BLOCK = register("grape_tree_block", new GrapeTreeBlock());
+	public static Block GRAPE_TREE_LOG = register("grape_tree_log", new GrapeTreeLogBlock());
 	public static Block GRAPE_TREE_LEAVES = register("grape_tree_leaves", new GrapeTreeLeavesBlock());
 
 	public static Block LEMON_TREE_BLOCK = register("lemon_tree_block", new LemonTreeBlock());
+	public static Block LEMON_TREE_LOG = register("lemon_tree_log", new LemonTreeLogBlock());
 	public static Block LEMON_TREE_LEAVES = register("lemon_tree_leaves", new LemonTreeLeavesBlock());
 
 	public static Block ORANGE_TREE_BLOCK = register("orange_tree_block", new OrangeTreeBlock());
+	public static Block ORANGE_TREE_LOG = register("orange_tree_log", new OrangeTreeLogBlock());
 	public static Block ORANGE_TREE_LEAVES = register("orange_tree_leaves", new OrangeTreeLeavesBlock());
 
 	public static Block PEACH_TREE_BLOCK = register("peach_tree_block", new PeachTreeBlock());
+	public static Block PEACH_TREE_LOG = register("peach_tree_log", new PeachTreeLogBlock());
 	public static Block PEACH_TREE_LEAVES = register("peach_tree_leaves", new PeachTreeLeavesBlock());
 
 	public static Block PEAR_TREE_BLOCK = register("pear_tree_block", new PearTreeBlock());
+	public static Block PEAR_TREE_LOG = register("pear_tree_log", new PearTreeLogBlock());
 	public static Block PEAR_TREE_LEAVES = register("pear_tree_leaves", new PearTreeLeavesBlock());
 
 	public static Block PLUM_TREE_BLOCK = register("plum_tree_block", new PlumTreeBlock());
-	public static Block PLUM_TREE_LEAVES = register("peach_tree_leaves", new PlumTreeLeavesBlock());
+	public static Block PLUM_TREE_LOG = register("plum_tree_log", new PlumTreeLogBlock());
+	public static Block PLUM_TREE_LEAVES = register("plum_tree_leaves", new PlumTreeLeavesBlock());
 
 	private static Block register(String key, Block blockIn) {
 		blockList.add(blockIn);
